c18f5978e8545acc087e02b03e57e2fb73ccaf20
Peyman Eskandari <peyman.github@gmail.com>
2019-07-26 16:21:00 +0430

    Initial commit

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>About</div>
+</template>
+
+<script>
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blog/_slug/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Blog single post
+    {{ $route.params.slug }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index",
+
+    data(){
+      return {
+        post: null,
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
